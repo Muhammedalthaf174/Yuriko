2aac79423f1aeba5073e2052d5b52787c4e3b58b
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-02-17 07:15:23 +0530

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -544,7 +544,7 @@ def Source_about_callback(update, context):
     query = update.callback_query
     if query.data == "source_":
         query.message.edit_text(
-            text=""" Hi..😻 I'm *yurikorobot*
+            text=""" Hi..😻 I'm *tezzarobot*
                  \nHere is the [🔥Source Code🔥](https://github.com/TeamDeeCode) .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
